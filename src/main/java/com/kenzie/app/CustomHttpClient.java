6057266000d8b99c9a75648f318c046bbdfe3496
Andrea <andrea.sorrels@mykenzie.snhu.edu>
2022-04-13 15:38:04 -0400

    Finishing Capstone

--- src/main/java/com/kenzie/app/CustomHttpClient.java
+++ src/main/java/com/kenzie/app/CustomHttpClient.java
@@ -1,18 +1,42 @@
 package com.kenzie.app;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class CustomHttpClient {
 
-    //TODO: Write sendGET method that takes URL and returns response
+    //DONE: Write sendGET method that takes URL and returns response
     public static String sendGET(String URLString) {
-        //** Start of GET request algorithm
 
-        return "";
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(URLString);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 200) {
+                return httpResponse.body();
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        catch (Exception e) {
+            return e.getMessage();
+        }
     }
 }
 
--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -1,23 +1,69 @@
 package com.kenzie.app;
 
 // import necessary libraries
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    /* Java Fundamentals Capstone project:
-       - Define as many variables, properties, and methods as you decide are necessary to
-       solve the program requirements.
-       - You are not limited to only the class files included here
-       - You must write the HTTP GET call inside the CustomHttpClient.sendGET(String URL) method
-         definition provided
-       - Your program execution must run from the main() method in Main.java
-       - The rest is up to you. Good luck and happy coding!
+    //Java Fundamentals Capstone project:
+    static String URL = "https://jservice.kenzie.academy/api/clues";
 
-     */
+    public static boolean checkAnswer(String userAnswer, String rightAnswer) {
+        if (userAnswer.equalsIgnoreCase(rightAnswer)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
+    public static void main(String[] args) throws JsonProcessingException {
+        try {
+            CustomHttpClient questionHttpClient = new CustomHttpClient();
+            ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args)  {
-        //Write main execution code here
+            String jsonQuestion = questionHttpClient.sendGET(URL);
+            QuestionDTO question = mapper.readValue(jsonQuestion, QuestionDTO.class);
+            List<Clues> questionList = question.getClues();
+
+            int numOfQuestions = 0;
+            int numOfCorrectAnswers = 0;
+
+            do {
+                try {
+                    Scanner scanner = new Scanner(System.in);
+                    int questionNumber = (int) (Math.random() * (questionList.size() - 1));
+                    Clues questionClue = questionList.get(questionNumber);
+                    Category questionCategory = questionClue.getCategory();
+
+                    System.out.println("Category: " + questionCategory.getTitle() + " " + "Question: " + questionClue.getQuestion());
+                    String participantsAnswer = scanner.nextLine();
+
+                    if (checkAnswer(participantsAnswer, questionClue.getAnswer()) == true) {
+                        numOfCorrectAnswers++;
+                        System.out.println("Correct! Current Score: " + numOfCorrectAnswers + "/10");
+                    } else {
+                        System.out.println("Incorrect. The correct answer was: " + questionClue.getAnswer() + ". Current Score: " + numOfCorrectAnswers + "/10");
+                    }
+
+                    numOfQuestions++;
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            while (numOfQuestions < 10);
+
+            System.out.println("All Done! Your total score: " + numOfCorrectAnswers + "/10");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
